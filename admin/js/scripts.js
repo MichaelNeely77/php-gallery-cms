f57aa7caffa89c7f9d464d67911c97770f6b7404
Michael Neely <michael@michaelneely.net>
2019-09-15 21:08:00 -0400

    Adding jQuery for modal functions

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,1 +1,18 @@
-tinymce.init({selector:'textarea'});
+$(document).ready(function() {
+
+    $(".modal_thumbnails").click(function() {
+        $("#set_user_image").prop('disabled', false);
+
+
+    });
+
+
+
+    tinymce.init({selector:'textarea'});
+});
+
+
+
+
+
+
