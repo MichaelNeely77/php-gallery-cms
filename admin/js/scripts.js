8d1276369094661aa72e86dae08d6cdb88543c5a
Michael Neely <michael@michaelneely.net>
2019-09-16 23:33:10 -0400

    Changing the AJAX PHP method

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -33,7 +33,9 @@ $(document).ready(function() {
             type: "POST",
             success: function(data) {
                 if(!data.error) {
-                    location.reload(true);
+
+                    $(".user_image_box a img").prop('src', data);
+
                 }
             }
         });
