57dc12ac7a9b89cb167f594492d2bcd9c7a3d569
Michael Neely <michael@michaelneely.net>
2019-09-17 00:28:26 -0400

    More AJAX and PHP

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -24,6 +24,18 @@ $(document).ready(function() {
         photo_id = $(this)
 .attr("data");
 
+$.ajax({
+    url: "includes/ajax_code.php",
+    data:{photo_id:photo_id},
+    type: "POST",
+    success:function(data) {
+        if(!data.error) {
+            $("#modal_sidebar").html(data);
+        }
+    }
+
+});
+
 
     });
 
@@ -40,17 +52,7 @@ $(document).ready(function() {
 
                     $(".user_image_box a img").prop('src', data);
 
-                    $.ajax({
-                        url: "includes/ajax_code.php",
-                        data:{photo_id:photo_id},
-                        type: "POST",
-                        success:function(data) {
-                            if(!data.error) {
-                                $("#modal_sidebar").html(data);
-                            }
-                        }
 
-                    });
 
                 }
             }
